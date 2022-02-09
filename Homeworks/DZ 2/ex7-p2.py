f456b65e79927ef05e2635fd9dbacf27262f3bcb
Alex <aleksmosya93@gmail.com>
2022-02-09 17:11:54 +0300

    ex 7

--- Homeworks/DZ 2/ex7-p2.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-method = {"method": "GET"}
-
-response4 = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-print(response4.request)
--- Homeworks/DZ 2/ex7.py
+++ Homeworks/DZ 2/ex7.py
@@ -1,27 +1,68 @@
 import requests
 
-print("---------HTTP-запрос любого типа без параметра method---------")
-response1 = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+method_list = [{"method": "GET"}, {"method": "POST"},
+               {"method": "PUT"}, {"method": "DELETE"}]
 
-print(response1.text + "\n")
+print("GET:")
+for method in method_list:
+    response_get = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+    print(f"В метод GET передан параметр {method['method']}: " + response_get.text)
+    if method['method'] == "GET":
+        if 'success' in response_get.text:
+            print("Методы совпадают, сервер тоже так считает. Все ОК\n")
+        else:
+            print("Методы совпадают, но сервер считает, что это не так\n")
+    else:
+        if 'success' in response_get.text:
+            print("Методы не совпадают, но сервер считает, что все ОК\n")
+        else:
+            print("Методы не совпадают и сервер так считает. Все ОК\n")
+print("\n")
 
-print("---------HTTP-запрос не из списка---------")
-response2 = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={'method': 'HEAD'})
+print("POST:")
+for method in method_list:
+    response_post = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+    print(f"В метод POST передан параметр {method['method']}: " + response_post.text)
+    if method['method'] == "POST":
+        if 'success' in response_post.text:
+            print("Методы совпадают, сервер тоже так считает. Все ОК\n")
+        else:
+            print("Методы совпадают, но сервер считает, что это не так\n")
+    else:
+        if 'success' in response_post.text:
+            print("Методы не совпадают, но сервер считает, что все ОК\n")
+        else:
+            print("Методы не совпадают и сервер так считает. Все ОК\n")
+print("\n")
 
-print(response2.text + "\n")
+print("PUT:")
+for method in method_list:
+    response_put = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+    print(f"В метод PUT передан параметр {method['method']}: " + response_put.text)
+    if method['method'] == "PUT":
+        if 'success' in response_put.text:
+            print("Методы совпадают, сервер тоже так считает. Все ОК\n")
+        else:
+            print("Методы совпадают, но сервер считает, что это не так\n")
+    else:
+        if 'success' in response_put.text:
+            print("Методы не совпадают, но сервер считает, что все ОК\n")
+        else:
+            print("Методы не совпадают и сервер так считает. Все ОК\n")
+print("\n")
 
-print("---------HTTP-запрос с правильным значением method---------")
-response3 = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={'method': 'POST'})
-
-print(response3.text + "\n")
-
-# print("---------HTTP-запрос---------")
-#
-# params_list = ["get", "post", "put", "delete"]
-# method_list = [{"method": "GET"}, {"method": "POST"}, {"method": "PUT"}, {"method": "DELETE"}]
-#
-# for i in params_list:
-#     for method in method_list:
-#         response4 = requests.i("https://playground.learnqa.ru/ajax/api/compare_query_type", params=param)
-#         if response4.starus_code == 200:
-#             print(f"method {i} with parameter params = {param} has following result {response4.result} with status code {response4.starus_code}")
+print("DELETE:")
+for method in method_list:
+    response_delete = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+    print(f"В метод DELETE передан параметр {method['method']}: " + response_delete.text)
+    if method['method'] == "DELETE":
+        if 'success' in response_delete.text:
+            print("Методы совпадают, сервер тоже так считает. Все ОК\n")
+        else:
+            print("Методы совпадают, но сервер считает, что это не так\n")
+    else:
+        if 'success' in response_delete.text:
+            print("Методы не совпадают, но сервер считает, что все ОК\n")
+        else:
+            print("Методы не совпадают и сервер так считает. Все ОК\n")
+print("\n")
