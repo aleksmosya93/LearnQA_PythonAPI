96aaee564c9a527ba916c31048878abadbc3ad39
aleksmosya93 <aleksmosya93@gmail.com>
2022-02-10 14:20:43 +0300

    parameterized_test_api

---
+++ Lessons/parameterized_test_api.py
@@ -0,0 +1,29 @@
+import requests
+import pytest
+
+class TestFirstApi:
+    names= [
+        ("Alex"),
+        ("Arseniy"),
+        ""
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello(self, name):
+        url = "https://playground.learnqa.ru/api/hello"
+        data = {'name': name}
+
+        response = requests.get(url, params=data)
+
+        assert response.status_code == 200, "Wrong response code"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no field 'answer' in the response"
+
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
+        actual_response_text = response_dict['answer']
+        assert actual_response_text == expected_response_text, "Actual text in the response is not corrext"
