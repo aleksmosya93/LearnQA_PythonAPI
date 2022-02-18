fc9d01931a21d85217f64e3f7efe0ad8d9a5f3d7
aleksmosya93 <aleksmosya93@gmail.com>
2022-02-18 18:23:44 +0300

    Lesson 4

--- Homeworks/lib/basecase.py
+++ Homeworks/lib/basecase.py
@@ -1,17 +1,18 @@
 import json
-
+from datetime import datetime
 from requests import Response
 
+
 class BaseCase:
-    def get_cookie (self, response: Response, cookie_name):
+    def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
-    def get_header (self, response: Response, headers_name):
+    def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find header with the name {headers_name} in the last response"
         return response.headers[headers_name]
 
-    def get_json_value (self, response: Response, name):
+    def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
