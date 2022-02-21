cc3dae0696b33d02b38fec8fef9c40e568ac6a27
aleksmosya93 <aleksmosya93@gmail.com>
2022-02-21 16:09:22 +0300

    Lesson 4. Logs

--- Lessons/tests/test_user_register.py
+++ Lessons/tests/test_user_register.py
@@ -1,4 +1,4 @@
-import requests
+from Lessons.lib.my_requests import MyRequests
 from Lessons.lib.base_case import BaseCase
 from Lessons.lib.assertions import Assertions
 
@@ -7,7 +7,7 @@ class TestUserRegister(BaseCase):
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
@@ -16,7 +16,7 @@ class TestUserRegister(BaseCase):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
